991339933aab09697a4c72af9316d6ea71944d3a
Jerry Martins <jerry.devw@gmail.com>
2023-07-05 11:55:56 -0400

    separa lista em pedacos de 100

--- src/main/java/cnab/normalization/bycoders/domain/usecase/ProcessCnabUseCaseImpl.java
+++ src/main/java/cnab/normalization/bycoders/domain/usecase/ProcessCnabUseCaseImpl.java
@@ -5,15 +5,20 @@ import cnab.normalization.bycoders.domain.usecase.api.ParseCnabFileUseCase;
 import cnab.normalization.bycoders.domain.usecase.api.ProcessCnabUseCase;
 import cnab.normalization.bycoders.domain.usecase.api.StoresCnabUseCase;
 import lombok.AllArgsConstructor;
+import org.apache.commons.collections4.ListUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class ProcessCnabUseCaseImpl implements ProcessCnabUseCase {
 
+    private static final int BATCH_SIZE = 100;
     private final ParseCnabFileUseCase parseCnabFileUseCase;
     private final StoresCnabUseCase storesCnabUseCase;
 
@@ -26,7 +31,11 @@ public class ProcessCnabUseCaseImpl implements ProcessCnabUseCase {
 
         Runnable runnable = () -> {
                     List<TransactionDomain> transactions = parseCnabFileUseCase.execute(multipartFile);
-                    storesCnabUseCase.execute(transactions);
+
+                    Collection<List<TransactionDomain>> transationsPartitioneds = new ArrayList<>(ListUtils
+                        .partition(new ArrayList<>(transactions), BATCH_SIZE));
+
+                    transationsPartitioneds.parallelStream().forEach(storesCnabUseCase::execute);
                 };
 
         Thread thread = new Thread(runnable);
