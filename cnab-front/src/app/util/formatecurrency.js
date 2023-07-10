573bc94c53591da5616abe7416cd332b3a6aeef2
Jerry Martins <jerry.devw@gmail.com>
2023-07-10 10:55:00 -0400

    nao faz o parse da moeda se for nulo

--- cnab-front/src/app/util/formatecurrency.js
+++ cnab-front/src/app/util/formatecurrency.js
@@ -1,5 +1,5 @@
 const FormateCurrency = (value) => {
-    return value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+    return value?.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 }
 
 export default FormateCurrency;
